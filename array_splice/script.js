3c3ba62e2db957c561b999cd7f63d245a42899d7
yinm <yinm.cminor9@gmail.com>
2018-03-17 19:42:34 +0900

    Check

--- array_splice/script.js
+++ array_splice/script.js
@@ -1,7 +1,7 @@
 //ref: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
 
-var myFish = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
-var removed = myFish.splice(3, 1);
+var myFish = ['angel', 'clown', 'drum', 'sturgeon'];
+var removed = myFish.splice(2, 1, 'trumpet');
 
 console.log(myFish);
 console.log(removed);
