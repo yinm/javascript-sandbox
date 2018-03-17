2b97431fcb896b7078f6ab52565880c5f159f34d
yinm <yinm.cminor9@gmail.com>
2018-03-17 19:59:20 +0900

    Check

--- object_assign/script.js
+++ object_assign/script.js
@@ -1,5 +1,31 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
 
-const obj = {a: 1};
-const copy = Object.assign({}, obj);
-console.log(copy);
+function test() {
+  'use strict'
+  
+  let obj1 = {a: 0, b: {c: 0}}
+  let obj2 = Object.assign({}, obj1)
+  console.log(JSON.stringify(obj2))
+
+  obj1.a = 1
+  console.log(JSON.stringify(obj1))
+  console.log(JSON.stringify(obj2))
+
+  obj2.a = 2
+  console.log(JSON.stringify(obj1))
+  console.log(JSON.stringify(obj2))
+
+  obj2.b.c = 3
+  console.log(JSON.stringify(obj1))
+  console.log(JSON.stringify(obj2))
+
+  // Deep Clone
+  obj1 = {a: 0, b: {c: 0}}
+  let obj3 = JSON.parse(JSON.stringify(obj1))
+  obj1.a = 4
+  obj1.b.c = 5
+  console.log(JSON.stringify(obj1))
+  console.log(JSON.stringify(obj3))
+}
+
+test()
