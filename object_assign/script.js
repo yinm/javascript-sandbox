e731a41f887d25df1046b1e21b346a47031efc61
yinm <yinm.cminor9@gmail.com>
2018-03-17 19:53:25 +0900

    Check

--- object_assign/script.js
+++ object_assign/script.js
@@ -1,14 +1,5 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
 
-const object1 = {
-  a: 1,
-  b: 2,
-  c: 3,
-};
-
-const object2 = Object.assign({}, object1);
-
-console.log(object2.c);
-
-console.log(object1);
-console.log(object2);
+const obj = {a: 1};
+const copy = Object.assign({}, obj);
+console.log(copy);
