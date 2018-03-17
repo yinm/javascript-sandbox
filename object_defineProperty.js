947c3ff30e243896b8f5cf1c0b6906107a575895
yinm <yinm.cminor9@gmail.com>
2018-03-17 21:29:55 +0900

    Check

--- object_defineProperty.js
+++ object_defineProperty.js
@@ -1,12 +1,28 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
 
-const object1 = {}
-
-Object.defineProperty(object1, 'property1', {
-  value: 42,
-  writable: false
+let o = {}
+Object.defineProperty(o, 'a', {
+  value: 1,
+  enumerable: true
 })
 
-object1.property1 = 77
+Object.defineProperty(o, 'b', {
+  value: 2,
+  enumerable: false
+})
 
-console.log(object1.property1)
+Object.defineProperty(o, 'c', {
+  value: 3
+})
+
+o.d = 4
+
+for (let i in o) {
+  console.log(i)
+}
+
+console.log(Object.keys(o))
+
+console.log(o.propertyIsEnumerable('a'))
+console.log(o.propertyIsEnumerable('b'))
+console.log(o.propertyIsEnumerable('c'))
