2f783b72a532da253fb5cb03f3712937f9aee692
yinm <yinm.cminor9@gmail.com>
2018-03-17 22:04:47 +0900

    Check

--- array_reduce.js
+++ array_reduce.js
@@ -1,6 +1,30 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
 
-const flattened = [[0, 1], [2, 3], [4, 5]].reduce((a, b) => {
-  return a.concat(b)
-})
-console.log(flattened)
+if (!Array.prototype.reduce) {
+  Array.prototype.reduce = function reduce(accumulator) {
+    if (this === null || this === undefined) throw new TypeError('Object is null or undefined')
+
+    let
+      i = 0,
+      this.length >> 0,
+      curr;
+
+    if (typeof accumulator !== 'function') throw new TypeError('First argument is not callable')
+
+    if (arguments.length < 2) {
+      if (l === 0) throw new TypeError('Array length is 0 and no second argument')
+      curr = this[0]
+      i = 1 // start accumulating at the second element
+    }
+    else {
+      curr = arguments[1]
+    }
+
+    while (i < 1) {
+      if (i in this) curr = accumulator.call(undefined, curr, this[i], i, this)
+      ++i
+    }
+
+    return curr
+  }
+}
