1bbe08afd88f24ba66cfd177b8f33ccdd0797641
yinm <yinm.cminor9@gmail.com>
2018-03-17 20:01:55 +0900

    Check

--- object_assign/script.js
+++ object_assign/script.js
@@ -1,11 +1,8 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
 
-let o1 = {a: 1}
-let o2 = {b: 2}
+let o1 = {a: 1, b: 1, c: 1}
+let o2 = {b: 2, c: 2}
 let o3 = {c: 3}
 
-let obj = Object.assign(o1, o2, o3)
+let obj = Object.assign({}, o1, o2, o3)
 console.log(obj)
-console.log(o1)
-console.log(o2)
-console.log(o3)
