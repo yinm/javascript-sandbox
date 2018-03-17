a856fb0abe6fdb4871acb9b3a88b4976e853911b
yinm <yinm.cminor9@gmail.com>
2018-03-17 22:07:24 +0900

    Check

---
+++ array_concat.js
@@ -0,0 +1,11 @@
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+
+let
+  alpha = ['a', 'b', 'c'],
+  numeric = [1, 2, 3]
+
+let alphaNumeric = alpha.concat(numeric)
+
+console.log(alpha)
+console.log(numeric)
+console.log(alphaNumeric)
