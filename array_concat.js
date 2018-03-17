2cf79e75b7a56d8cb975823b1e1a9f5662c97495
yinm <yinm.cminor9@gmail.com>
2018-03-17 22:09:01 +0900

    Check

--- array_concat.js
+++ array_concat.js
@@ -1,9 +1,6 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
 
-let
-  num1 = [1, 2, 3],
-  num2 = [4, 5, 6],
-  num3 = [7, 8, 9]
+let alpha = ['a', 'b', 'c']
+let alphaNumeric = alpha.concat(1, [2, 3])
 
-let nums = num1.concat(num2, num3)
-console.log(nums)
+console.log(alphaNumeric)
