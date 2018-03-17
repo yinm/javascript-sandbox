857bfe76b6c5e8a2d39814d3b8865d5095c44f8d
yinm <yinm.cminor9@gmail.com>
2018-03-17 21:57:23 +0900

    Check

--- array_reduce.js
+++ array_reduce.js
@@ -1,8 +1,7 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
 
-const array1 = [1, 2, 3, 4]
-const reducer = (accumulator, currentValue) => accumulator + currentValue
-
-console.log(array1.reduce(reducer))
-
-console.log(array1.reduce(reducer, 5))
+console.log(
+  [0,1,2,3,4].reduce((previousValue, currentValue, index, array) => {
+    return previousValue + currentValue
+  })
+)
