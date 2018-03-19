0f52f75dcdc8b598a5d86d75c7f10b0622ebfe32
yinm <yinm.cminor9@gmail.com>
2018-03-19 23:14:29 +0900

    Check

--- json_stringify.js
+++ json_stringify.js
@@ -1,7 +1,17 @@
-console.log(JSON.stringify({x: 5, y: 6}))
-
-console.log(JSON.stringify([new Number(3), new String('false'), new Boolean(false)]))
-
-console.log(JSON.stringify({x: [10, undefined, function(){}, Symbol('')]}))
-
+console.log(JSON.stringify({}))
+console.log(JSON.stringify(true))
+console.log(JSON.stringify('foo'))
+console.log(JSON.stringify([1, 'false', false]))
+console.log(JSON.stringify({x: 5}))
 console.log(JSON.stringify(new Date(2006, 0, 2, 15, 4, 5)))
+
+console.log(JSON.stringify({x: undefined, y: Object, z: Symbol('')}))
+console.log(JSON.stringify({[Symbol('foo')]: 'foo'}))
+console.log(JSON.stringify({[Symbol.for('foo')]: 'foo'}, [Symbol.for('foo')]))
+console.log(JSON.stringify({[Symbol.for('foo')]: 'foo'}, function(k, v) {
+  if (typeof k === 'symbol') {
+    return 'a symbol'
+  }
+}))
+
+console.log(JSON.stringify(Object.create(null, { x: {value: 'x', enumerable: false}, y: {value: 'y', enumerable: true} })))
