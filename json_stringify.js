9821ee4a8835b51d00ddf375cae881cb0545d057
yinm <yinm.cminor9@gmail.com>
2018-03-19 23:17:46 +0900

    Check replacer wtih array

--- json_stringify.js
+++ json_stringify.js
@@ -1,10 +1,2 @@
-function replacer(key, value) {
-  // Filtering out properties
-  if (typeof value === 'string') {
-    return undefined
-  }
-  return value
-}
-
 const foo = {foundation: 'Mozilla', model: 'box', week: 45, transport: 'car', month: 7}
-console.log(JSON.stringify(foo, replacer))
+console.log(JSON.stringify(foo, ['week', 'month']))
