7ce991aff131a53b753d6d5ff05576c6797bfc16
yinm <yinm.cminor9@gmail.com>
2018-03-19 23:16:29 +0900

    Check replacer

--- json_stringify.js
+++ json_stringify.js
@@ -1,17 +1,10 @@
-console.log(JSON.stringify({}))
-console.log(JSON.stringify(true))
-console.log(JSON.stringify('foo'))
-console.log(JSON.stringify([1, 'false', false]))
-console.log(JSON.stringify({x: 5}))
-console.log(JSON.stringify(new Date(2006, 0, 2, 15, 4, 5)))
-
-console.log(JSON.stringify({x: undefined, y: Object, z: Symbol('')}))
-console.log(JSON.stringify({[Symbol('foo')]: 'foo'}))
-console.log(JSON.stringify({[Symbol.for('foo')]: 'foo'}, [Symbol.for('foo')]))
-console.log(JSON.stringify({[Symbol.for('foo')]: 'foo'}, function(k, v) {
-  if (typeof k === 'symbol') {
-    return 'a symbol'
+function replacer(key, value) {
+  // Filtering out properties
+  if (typeof value === 'string') {
+    return undefined
   }
-}))
+  return value
+}
 
-console.log(JSON.stringify(Object.create(null, { x: {value: 'x', enumerable: false}, y: {value: 'y', enumerable: true} })))
+const foo = {foundation: 'Mozilla', model: 'box', week: 45, transport: 'car', month: 7}
+console.log(JSON.stringify(foo, replacer))
