d71040d4d77be0810a678ae8d925e77ac75f06d3
yinm <yinm.cminor9@gmail.com>
2018-03-21 17:04:35 +0900

    Check same origin policy

---
+++ xml_http_request/server.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const fs = require('fs')
+const server = http.createServer()
+
+server.on('request', (req, res) => {
+  fs.readFile(`${__dirname}/example.txt`, 'utf-8', (err, data) => {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/plain'})
+      res.write('Not Found')
+      return res.end()
+    }
+
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.write(data)
+    res.end()
+  })
+})
+
+server.listen(8888)
+console.log('Server running at http://localhost:8888/')
