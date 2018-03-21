abb064bb1be14065d6fa989218c8af4757669eb2
yinm <yinm.cminor9@gmail.com>
2018-03-21 16:49:34 +0900

    Check rendering html

--- nodejs_server2/server.js
+++ nodejs_server2/server.js
@@ -1,28 +1,22 @@
 const http = require('http')
 const settings = require('./settings')
-
-console.log(settings)
+const fs = require('fs')
 
 const server = http.createServer()
 
 server.on('request', (req, res) => {
-  let message
+  fs.readFile(`${__dirname}/views/index.html`, 'utf-8', (err, data) => {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/plain'})
+      res.write('Not Found')
+      return res.end()
+    }
 
-  switch (req.url) {
-    case '/about':
-      message = 'about this page'
-      break
-    case '/profile':
-      message = 'about me'
-      break
-    default:
-      message = 'wrong page'
-      break
-  }
-
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.write(message)
-  res.end()
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(data)
+    res.end()
+  })
 })
 
+console.log(settings)
 server.listen(settings.port, settings.host)
