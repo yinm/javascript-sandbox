63721c4b91e1b3ebef8244170689e3c9367bc871
yinm <yinm.cminor9@gmail.com>
2018-03-21 17:08:47 +0900

    Add current origin to Access-Control-Allow-Origin header

--- xml_http_request/server.js
+++ xml_http_request/server.js
@@ -12,7 +12,7 @@ server.on('request', (req, res) => {
 
     res.writeHead(200, {
       'Content-Type': 'text/plain',
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': 'http://172.16.101.30:55556'
     })
     res.write(data)
     res.end()
