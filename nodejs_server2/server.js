82b2122c01f5a69bb2bdfbf90192553200524a75
yinm <yinm.cminor9@gmail.com>
2018-03-21 16:44:55 +0900

    Check switch rendering page

--- nodejs_server2/server.js
+++ nodejs_server2/server.js
@@ -6,8 +6,22 @@ console.log(settings)
 const server = http.createServer()
 
 server.on('request', (req, res) => {
+  let message
+
+  switch (req.url) {
+    case '/about':
+      message = 'about this page'
+      break
+    case '/profile':
+      message = 'about me'
+      break
+    default:
+      message = 'wrong page'
+      break
+  }
+
   res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.write('hello Node.js!!!!!!!!!!!!')
+  res.write(message)
   res.end()
 })
 
