90a56d6e9a6cee2fed6d1ca8b8c8afe7e3ac6201
yinm <yinm.cminor9@gmail.com>
2018-03-24 14:10:49 +0900

    Check escape()

---
+++ escape.js
@@ -0,0 +1,6 @@
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/escape
+console.log(escape('abc123'))
+console.log(escape('äöü'))
+console.log(escape('あいうえお'))
+
+console.log(escape('@*_+-./'))
