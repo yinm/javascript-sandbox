665c08131456f98db0427a9f138f375d0f6cc2d8
yinm <yinm.cminor9@gmail.com>
2018-03-24 15:33:10 +0900

    Check JSON response

---
+++ xml_http_request2/test.php
@@ -0,0 +1,8 @@
+<?php
+$name = (isset($_POST['userName'])) ? $_POST['userName'] : 'no name';
+$computedString = "Hi, {$name}";
+$array = [
+    'userName' => $name,
+    'computedString' => $computedString,
+];
+echo json_encode($array);
