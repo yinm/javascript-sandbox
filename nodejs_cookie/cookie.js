150acb5615df104e763924435793c1651b03d2cb
yinm <yinm.cminor9@gmail.com>
2018-03-26 09:11:17 +0900

    Check cookie module

--- nodejs_cookie/cookie.js
+++ nodejs_cookie/cookie.js
@@ -3,7 +3,8 @@ const
   http = require('http'),
   fs = require('fs'),
   ejs = require('ejs'),
-  url = require('url')
+  url = require('url'),
+  cookie = require('cookie')
 
 const index1 = fs.readFileSync('./index1.ejs', 'utf8')
 
@@ -16,13 +17,29 @@ function doRequest(req, res) {
 
   switch (path.pathname) {
     case '/':
-      const cookie = req.headers.cookie
+      const parsed_cookie = cookie.parse(req.headers.cookie)
+
       const view = ejs.render(index1, {
         title: 'Cookie Sample Page',
-        msg: `Cookie: ${cookie}`
+        msg: `Cookie:${parsed_cookie.lastURL}, ${parsed_cookie.lastTime}`
       })
       res.setHeader('Content-Type', 'text/html')
       res.write(view)
+      res.end()
+      break
+
+    case '/set_time':
+      const date = new Date().toDateString()
+      const serializedCookie1 = cookie.serialize(
+        'lastTime',
+        `アクセス日時:${date}`,
+        {
+          maxAge: 100
+        }
+      )
+
+      res.setHeader('Set-Cookie', serializedCookie1)
+      res.write('Cookie lastTime set!!1')
       res.end()
       break
 
@@ -32,7 +49,15 @@ function doRequest(req, res) {
     default:
       res.setHeader('Content-Type', 'text/plain')
 
-      res.setHeader('Set-Cookie', [`lastURL=${path.pathname}`])
+      const serializedCookie2 = cookie.serialize(
+        'lastURL',
+        path.pathname,
+        {
+          maxAge: 100
+        }
+      )
+
+      res.setHeader('Set-Cookie', serializedCookie2)
       res.write('Set Cookie!!1')
       res.end()
       break
