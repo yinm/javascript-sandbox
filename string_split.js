101874d2b4255ed919569d62e8ee194653359216
yinm <yinm.cminor9@gmail.com>
2018-03-28 08:58:47 +0900

    Check limit

--- string_split.js
+++ string_split.js
@@ -1,6 +1,2 @@
-const names = "Harry Trump ;Fred Barney; Helen Rigby ; Bill Abel ;Chris Hand "
-console.log(names)
-
-const re = /\s*;\s*/
-const nameList = names.split(re)
-console.log(nameList)
+const myString = 'Hello World. How are you doing?'
+console.log(myString.split(" ", 3))
