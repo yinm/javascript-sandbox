3911d73bffdb707d44f2397e94b6383d4633c3a5
yinm <yinm.cminor9@gmail.com>
2018-03-29 10:33:29 +0900

    Check lookup

--- regexp_exec.js
+++ regexp_exec.js
@@ -1,12 +1,27 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
 
-const myReg = /ab*/g
-const str = 'abbcdefabh'
-console.log(`str.length: ${str.length}`)
+const A = ["Frank", "Emily", "Jane", "Harry", "Nick", "Beth", "Rick",
+  "Terrence", "Carol", "Ann", "Terry", "Frank", "Alice", "Rick",
+  "Bill", "Tom", "Fiona", "Jane", "William", "Joan", "Beth"];
 
-let myArray
-while ((myArray = myReg.exec(str)) !== null) {
-  let message = `${myArray[0]} を見つけました。`
-  message += `次のマッチは ${myReg.lastIndex} からです`
-  console.log(message)
+function lookup(input) {
+  const firstName = /\w+/i.exec(input.value)
+  if (!firstName) {
+    console.log(`${input.value} isn't a name!`)
+    return
+  }
+
+  let count = 0
+  for (let i = 0; i < A.length; i++) {
+    if (firstName[0].toLowerCase() == A[i].toLowerCase()) {
+      count++
+    }
+  }
+
+  const midstring = count == 1
+    ? " other has "
+    : " others have "
+  console.log(`Thanks, ${count}${midstring}the same name!`)
 }
+
+lookup({value: "Rick"})
