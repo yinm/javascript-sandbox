73d63da9d8714b6aa58108b33557e9a2ae2770d0
yinm <yinm.cminor9@gmail.com>
2018-03-30 22:52:31 +0900

    Check replacer

--- json_stringify2.js
+++ json_stringify2.js
@@ -1,8 +1,16 @@
 // http://www.task-notes.com/entry/20160719/1468858991
 
-console.log(JSON.stringify({}))
-console.log(JSON.stringify({ x: 10, y: undefined, z: 20 }))
-console.log(JSON.stringify("foo"))
-console.log(JSON.stringify(true))
-console.log(JSON.stringify([1, "false", false, undefined]))
-console.log(JSON.stringify(null))
+const json = JSON.stringify({"one": 1, "two": 2, "three": true}, (key, value) => {
+  console.log(key)
+  console.log(value)
+
+  if (key === '') return value
+
+  if (Number.isFinite(value)) {
+    return String(value * 2)
+  } else {
+    return undefined
+  }
+})
+
+console.log(json)
