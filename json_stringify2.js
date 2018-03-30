ab47a88dc774c3fc3b7b2a3b5f104e4fe91fc71f
yinm <yinm.cminor9@gmail.com>
2018-03-30 22:49:33 +0900

    Check JSON.stringify()

---
+++ json_stringify2.js
@@ -0,0 +1,8 @@
+// http://www.task-notes.com/entry/20160719/1468858991
+
+console.log(JSON.stringify({}))
+console.log(JSON.stringify({ x: 10, y: undefined, z: 20 }))
+console.log(JSON.stringify("foo"))
+console.log(JSON.stringify(true))
+console.log(JSON.stringify([1, "false", false, undefined]))
+console.log(JSON.stringify(null))
