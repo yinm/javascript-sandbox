550c159f9f2190ab15cf8d946ba981ed8689b5d6
yinm <yinm.cminor9@gmail.com>
2018-03-30 22:54:19 +0900

    Check replacer with array

--- json_stringify2.js
+++ json_stringify2.js
@@ -1,16 +1,3 @@
 // http://www.task-notes.com/entry/20160719/1468858991
 
-const json = JSON.stringify({"one": 1, "two": 2, "three": true}, (key, value) => {
-  console.log(key)
-  console.log(value)
-
-  if (key === '') return value
-
-  if (Number.isFinite(value)) {
-    return String(value * 2)
-  } else {
-    return undefined
-  }
-})
-
-console.log(json)
+console.log(JSON.stringify({"one": 1, "two": 2, "three": true}, ["two", "three"]))
