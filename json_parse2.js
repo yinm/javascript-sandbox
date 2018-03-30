59cacd625737474498b001ce902f3a37d75d17bb
yinm <yinm.cminor9@gmail.com>
2018-03-30 09:20:54 +0900

    Check reviver

--- json_parse2.js
+++ json_parse2.js
@@ -1,9 +1,13 @@
 // http://www.task-notes.com/entry/20160719/1468858991
 
-console.log(JSON.parse('{}'))
-console.log(JSON.parse('{"bar": 1234, "foo": "baz"}'))
-console.log(JSON.parse('"foo"'))
-console.log(JSON.parse('true'))
-console.log(JSON.parse('[1, 5, false]'))
-console.log(JSON.parse('null'))
-console.log(JSON.parse('[{"bar": 1234, "foo": "baz"}, {"bar": 5678, "foo": "hoge"}]'))
+const parsedJson = JSON.parse('{"one": 1, "two": 2, "three": true}', (key, value) => {
+  console.log(key)
+  if (key === '') return value
+  if (Number.isFinite(value)) {
+    return String(value * 2)
+  } else {
+    return undefined  // return only integer
+  }
+})
+
+console.log(parsedJson)
