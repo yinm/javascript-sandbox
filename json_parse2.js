1466166d2f8ad5a891e68ac59ecaeadf4255e87a
yinm <yinm.cminor9@gmail.com>
2018-03-30 09:16:40 +0900

    Check JSON.parse()

---
+++ json_parse2.js
@@ -0,0 +1,9 @@
+// http://www.task-notes.com/entry/20160719/1468858991
+
+console.log(JSON.parse('{}'))
+console.log(JSON.parse('{"bar": 1234, "foo": "baz"}'))
+console.log(JSON.parse('"foo"'))
+console.log(JSON.parse('true'))
+console.log(JSON.parse('[1, 5, false]'))
+console.log(JSON.parse('null'))
+console.log(JSON.parse('[{"bar": 1234, "foo": "baz"}, {"bar": 5678, "foo": "hoge"}]'))
