991307fd47caf5bf659da9311dbd40b398d956e6
yinm <yinm.cminor9@gmail.com>
2018-03-30 22:55:28 +0900

    Check space

--- json_stringify2.js
+++ json_stringify2.js
@@ -1,3 +1,4 @@
 // http://www.task-notes.com/entry/20160719/1468858991
 
-console.log(JSON.stringify({"one": 1, "two": 2, "three": true}, ["two", "three"]))
+console.log(JSON.stringify({one: 1, two: 2}, null, 2))
+console.log(JSON.stringify({one: 1, two: 2}, null, '\t'))
