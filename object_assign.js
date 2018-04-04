bb4e324ce46abb5830330f909f37cb368ee8a89c
yinm <yinm.cminor9@gmail.com>
2018-04-04 23:44:15 +0900

    Check abort

--- object_assign.js
+++ object_assign.js
@@ -1,10 +1,17 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
 
-const
-  v1 = 'abc',
-  v2 = true,
-  v3 = 10,
-  v4 = Symbol('foo')
+const target = Object.defineProperty({}, 'foo', {
+  value: 1,
+  writable: false
+})
 
-const obj = Object.assign({}, v1, null, v2, undefined, v3, v4)
-console.log(obj)
+// TypeError: Cannot assign to read only property 'foo' of object '#<Object>'
+// Object.assign(target, { bar: 2 }, { foo2: 3, foo: 3, foo3: 3 }, { baz: 4 })
+Object.assign(target, { bar: 2 }, { foo2: 3, foo3: 3 }, { baz: 4 })
+
+console.log(target.bar)
+console.log(target.foo2)
+console.log(target.foo)
+console.log(target.foo3)
+console.log(target.baz)
+console.log(target)
