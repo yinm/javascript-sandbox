b40febe8e243c67b8e4aa619de2a8b75aae1e1de
yinm <yinm.cminor9@gmail.com>
2018-04-04 22:53:46 +0900

    Check Filtering invalid entries from JSON

--- array_filter.js
+++ array_filter.js
@@ -1,8 +1,31 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 
-function isBigEnough(value) {
-  return value >= 10
+const arr = [
+  { id: 15 },
+  { id: -1 },
+  { id: 0 },
+  { id: 3 },
+  { id: 12.2 },
+  { },
+  { id: null },
+  { id: NaN },
+  { id: 'undefined' },
+]
+
+let invalidEntries = 0
+
+function isNumber(obj) {
+  return obj !== undefined && typeof(obj) === 'number' && !isNaN(obj)
 }
 
-const filterd = [12, 5, 8, 130, 44].filter(isBigEnough)
-console.log(filterd)
+function filterByID(item) {
+  if (isNumber(item.id) && item.id !== 0) {
+    return true
+  }
+  invalidEntries++
+  return false
+}
+
+const arrByID = arr.filter(filterByID)
+console.log('Filtered Array\n', arrByID)
+console.log('Number of Invalid Entries = ', invalidEntries)
