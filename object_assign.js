b5ab7ef272ca934ad872380e7311d67f5632b01b
yinm <yinm.cminor9@gmail.com>
2018-04-04 23:28:58 +0900

    Check merging object with same property

--- object_assign.js
+++ object_assign.js
@@ -1,12 +1,9 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
 
 let
-  o1 = {a: 1},
-  o2 = {b: 2},
-  o3 = {c: 3}
+  o1 = { a: 1, b: 1, c: 1 },
+  o2 = { b: 2, c: 2 },
+  o3 = { c: 3 }
 
-const obj = Object.assign(o1, o2, o3)
+const obj = Object.assign({}, o1, o2, o3)
 console.log(obj)
-console.log(o1)
-console.log(o2)
-console.log(o3)
