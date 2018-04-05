093edd29e14d393717d3bcc96ce8e2e0d6f9268e
yinm <yinm.cminor9@gmail.com>
2018-04-05 09:25:19 +0900

    Check sort of object

--- array_sort.js
+++ array_sort.js
@@ -1,3 +1,34 @@
-const numbers = [4, 2, 5, 1, 3]
-numbers.sort((a, b) => a - b)
-console.log(numbers)
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
+const items = [
+  { name: 'Edward', value: 21 },
+  { name: 'Sharpe', value: 37 },
+  { name: 'And', value: 45 },
+  { name: 'The', value: -12 },
+  { name: 'Magnetic', value: 13 },
+  { name: 'Zeros', value: 37 },
+]
+
+// sort by value
+items.sort((a, b) => a.value - b.value)
+
+console.log(items)
+
+// sort by name
+items.sort((a, b) => {
+  const nameA = a.name.toUpperCase()  // ignore upper and lowercase
+  const nameB = b.name.toUpperCase()  // ignore upper and lowercase
+
+  if (nameA < nameB) {
+    return -1
+  }
+
+  if (nameA > nameB) {
+    return 1
+  }
+
+  // names must be equal
+  return 0
+})
+
+console.log(items)
