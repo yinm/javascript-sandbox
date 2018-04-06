d7e762a818d44d55a999fb1721046b0cf3db5f82
yinm <yinm.cminor9@gmail.com>
2018-04-06 10:48:04 +0900

    Check sorting with map

--- array_sort.js
+++ array_sort.js
@@ -1,5 +1,27 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 
-const items = ['réservé', 'premier', 'cliché', 'communiqué', 'café', 'adieu']
-items.sort((a, b) => a.localeCompare(b))
-console.log(items)
+// the array to be sorted
+const list = ['Delta', 'alpha', 'CHARLIE', 'bravo']
+
+// temporary array holds objects with position and sort-value
+const mapped = list.map((el, i) => {
+  return { index: i, value: el.toLowerCase() }
+})
+console.log(mapped)
+
+// sorting the mapped array containing the reduced values
+mapped.sort((a, b) => {
+  if (a.value > b.value) {
+    return 1
+  }
+  if (a.value < b.value) {
+    return -1
+  }
+
+  return 0
+})
+console.log(mapped)
+
+// container for the resulting order
+const result = mapped.map((el) => list[el.index])
+console.log(result)
