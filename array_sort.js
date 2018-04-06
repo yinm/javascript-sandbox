9b5ead8f5b368029ccc8caffd9770f53a360a8d8
yinm <yinm.cminor9@gmail.com>
2018-04-06 10:51:36 +0900

    Check 2nd

--- array_sort.js
+++ array_sort.js
@@ -1,15 +1,12 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 
-// the array to be sorted
+//2nd
 const list = ['Delta', 'alpha', 'CHARLIE', 'bravo']
 
-// temporary array holds objects with position and sort-value
 const mapped = list.map((el, i) => {
   return { index: i, value: el.toLowerCase() }
 })
-console.log(mapped)
 
-// sorting the mapped array containing the reduced values
 mapped.sort((a, b) => {
   if (a.value > b.value) {
     return 1
@@ -22,6 +19,5 @@ mapped.sort((a, b) => {
 })
 console.log(mapped)
 
-// container for the resulting order
 const result = mapped.map((el) => list[el.index])
 console.log(result)
