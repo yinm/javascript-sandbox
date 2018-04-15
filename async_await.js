068f693a2be43d42f312949ea20b5eb910b5d786
yinm <yinm.cminor9@gmail.com>
2018-04-15 14:31:12 +0900

    Check async/await

--- async_await.js
+++ async_await.js
@@ -3,29 +3,21 @@
 function sampleResolve(value) {
   return new Promise(resolve => {
     setTimeout(() => {
-      resolve(value * 2)
+      resolve(value)
     }, 1000)
   })
 }
 
-function sample() {
-  let result = 0
-
-  return sampleResolve(5)
-    .then(value => {
-      result += value
-      return sampleResolve(10)
-    })
-    .then(value => {
-      result += value
-      return sampleResolve(20)
-    })
-    .then(value => {
-      result += value
-      return result
-    })
+async function sample() {
+  return await sampleResolve(5) * await sampleResolve(10) + await sampleResolve(20)
 }
 
-sample().then(v => {
-  console.log(v)
-})
+async function sample2() {
+  const a = await sampleResolve(5)
+  const b = await sampleResolve(10)
+  const c = await sampleResolve(20)
+  return a * b + c
+}
+
+sample().then(v => console.log(v))
+sample2().then(v => console.log(v))
