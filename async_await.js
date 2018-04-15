c5efd3c06a75cd9b4b287064352c4a4134d88f64
yinm <yinm.cminor9@gmail.com>
2018-04-15 15:49:55 +0900

    Check async/await with array.map()

--- async_await.js
+++ async_await.js
@@ -8,20 +8,9 @@ function sampleResolve(value) {
   })
 }
 
-function sampleResolve2(value) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(value * 2)
-    }, 1000)
-  })
-}
-
-
 async function sample() {
-  const [a, b] = await Promise.all([sampleResolve(5), sampleResolve(10)])
-  const c = await sampleResolve2(b)
-
-  return [a, b, c]
+  const array = [5, 10, 15]
+  return await Promise.all(array.map(async value => await sampleResolve(value) * 2))
 }
 
 sample().then(([a, b, c]) => console.log(a, b, c))
