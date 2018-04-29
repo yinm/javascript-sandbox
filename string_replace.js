0503f8e541a37c751e572727b5af17f16c2a04ce
yinm <yinm.cminor9@gmail.com>
2018-04-29 20:07:34 +0900

    Check replace

--- string_replace.js
+++ string_replace.js
@@ -1,10 +1,22 @@
 // https://qiita.com/hrdaya/items/291276a5a20971592216
 
-function tab2space(str, num) {
-  num = parseInt(num, 10)
-  const space = new Array(isNaN(num) ? 5 : num + 1).join(' ')
-  return str.replace(/\t/g, space)
+function snake2camel(str, upper) {
+  str = str
+    .replace(/^[\-_ ]/g, '')
+    .replace(/[\-_ ]./g, (match) => {
+      return match.charAt(1).toUpperCase()
+    })
+
+  return upper === true
+    ? str.replace(/^[a-z]/, (match) => {
+        return match.toUpperCase()
+      })
+    : str
 }
 
-console.log(tab2space('foo\tbar', 'a'))
-console.log(tab2space('foo\tbar', 10))
+console.log(snake2camel('abc-def', false))
+console.log(snake2camel('abc-def', true))
+console.log(snake2camel('abc_def', false))
+console.log(snake2camel('abc_def', true))
+console.log(snake2camel('abc def', false))
+console.log(snake2camel('abc def', true))
