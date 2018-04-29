f46c11e6f761fcc5d953726109b1380e73684e2a
yinm <yinm.cminor9@gmail.com>
2018-04-29 20:37:01 +0900

    Check only executing on inherit properties

--- object_hasOwnProperty.js
+++ object_hasOwnProperty.js
@@ -1,15 +1,8 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwnProperty
 
 const o = new Object()
-o.prop = 'exitsts'
 
+o.prop = 'exists'
 console.log(o.hasOwnProperty('prop'))
-
-function changePropName(obj, propNameStr, newPropNameStr) {
-  o[newPropNameStr] = o[propNameStr]
-  delete o[propNameStr]
-}
-
-changePropName(o, 'prop', 'newProp')
-console.log(o.hasOwnProperty('prop'))
-console.log(o.hasOwnProperty('newProp'))
+console.log(o.hasOwnProperty('toString'))
+console.log(o.hasOwnProperty('hasOwnProperty'))
