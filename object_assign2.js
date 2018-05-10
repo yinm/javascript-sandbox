aad1be504e288e5621229a0b67485bc99a0b8081
yinm <yinm.cminor9@gmail.com>
2018-05-10 23:18:19 +0900

    Check Object.assign() with constructor

--- object_assign2.js
+++ object_assign2.js
@@ -1,18 +1,12 @@
 // http://js-next.hatenablog.com/entry/2015/05/07/211859
 
-const sym = Symbol()
-
-let obj1 = {
-  "str": 1,
+class Cat {
+  constructor(name, age, color) {
+    Object.assign(this, { name, age, color })
+  }
 }
 
-let obj2 = {
-  "str": 2,
-  [sym]: 2,
-}
+const tama = new Cat('たま', 9, 'もも')
 
-const obj3 = Object.assign(obj1, obj2)
-
-console.log(obj1 === obj3)
-console.log(obj1["str"] === 2)
-console.log(obj1[sym] === 2)
+console.log(tama)
+console.log(tama.name === 'たま')
