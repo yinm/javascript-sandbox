819183df4b971766ca8fbc6ff5b6cc02a51e83f5
yinm <yinm.cminor9@gmail.com>
2018-05-11 10:33:52 +0900

    Check Object.getOwnPropertyDescriptor()

--- object_getOwnPropertyDescriptor.js
+++ object_getOwnPropertyDescriptor.js
@@ -2,15 +2,15 @@
 
 let o, d
 
-o = { get foo() {return 17} }
+o = {get foo() { return 17 }}
 d = Object.getOwnPropertyDescriptor(o, 'foo')
 console.log(d)
 
-o = {bar: 42}
+o = { bar: 42 }
 d = Object.getOwnPropertyDescriptor(o, 'bar')
 console.log(d)
 
 o = {}
-Object.defineProperty(o, 'baz', { value: 8675309, writable: false, enumerable: false })
+Object.defineProperty(o, 'baz', { value: 8675390, writable: false, enumerable: false })
 d = Object.getOwnPropertyDescriptor(o, 'baz')
 console.log(d)
