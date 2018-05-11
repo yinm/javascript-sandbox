7d2308fcd5d2fbb063b31437111f5c87bbad0e08
yinm <yinm.cminor9@gmail.com>
2018-05-11 10:00:15 +0900

    Check cloning an object

--- object_assign.js
+++ object_assign.js
@@ -1,12 +1,5 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
 
-const object1 = {
-  a: 1,
-  b: 2,
-  c: 3,
-}
-
-const object2 = Object.assign({c: 4, d: 5,}, object1)
-
-console.log(object1)
-console.log(object2)
+const obj = {a: 1}
+const copy = Object.assign({}, obj)
+console.log(copy)
