46eca33c0f57becf8424863d4566d70366e0bbb9
yinm <yinm.cminor9@gmail.com>
2018-05-11 10:06:04 +0900

    Check merging objects

--- object_assign.js
+++ object_assign.js
@@ -1,29 +1,9 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
 
-function test() {
-  let obj1 = {a: 0, b: {c: 0}}
-  let obj2 = Object.assign({}, obj1)
-  console.log(JSON.stringify(obj2))
+let o1 = {a: 1}
+let o2 = {b: 2}
+let o3 = {c: 3}
 
-  obj1.a = 1
-  console.log(JSON.stringify(obj1))
-  console.log(JSON.stringify(obj2))
-
-  obj2.a = 2
-  console.log(JSON.stringify(obj1))
-  console.log(JSON.stringify(obj2))
-
-  obj2.b.c = 3
-  console.log(JSON.stringify(obj1))
-  console.log(JSON.stringify(obj2))
-
-  // Deep Clone
-  obj1 = {a: 0, b: {c: 0}}
-  let obj3 = JSON.parse(JSON.stringify(obj1))
-  obj1.a = 4
-  obj1.b.c = 3
-  console.log(JSON.stringify(obj1))
-  console.log(JSON.stringify(obj3))
-}
-
-test()
+const obj = Object.assign(o1, o2, o3)
+console.log(obj)
+console.log(o1)
