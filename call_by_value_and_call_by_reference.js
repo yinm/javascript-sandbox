b89c8bd4b9a1e8ecc1d2bff2018f0ba71a5a1bf7
yinm <yinm.cminor9@gmail.com>
2018-05-12 10:28:03 +0900

    Check new Object()

--- call_by_value_and_call_by_reference.js
+++ call_by_value_and_call_by_reference.js
@@ -1,9 +1,9 @@
 // https://qiita.com/migi/items/3417c2de685c368faab1
 
 let a, b
-a = ['hoge', 'fuga']
+a = {hoge: 'hogehoge', fuga: 'fugafuga'}
 b = a
-// b = []
-b = new Array()
+// b = {}
+b = new Object()
 console.log(b)
 console.log(a)
