2847321f6f5359f2aa482551a881d46839d3571a
yinm <yinm.cminor9@gmail.com>
2018-05-12 10:32:17 +0900

    Check call by value with array

--- call_by_value_and_call_by_reference.js
+++ call_by_value_and_call_by_reference.js
@@ -1,14 +1,10 @@
 // https://qiita.com/migi/items/3417c2de685c368faab1
 
 let a, b
-a = 0
-b = a
-b.hoge = 'hoge'
+a = [0, 1, 2]
+b = a.concat()
+console.log(b)
 
-console.log(a.hoge)
-console.log(b.hoge)
-
-let c
-c = 0
-c.hoge = 'hoge'
-console.log(c.hoge)
+b[0] = 5
+console.log(b)
+console.log(a)
