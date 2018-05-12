caa5a956923e88c6242d01ada811818bf60e7b0d
yinm <yinm.cminor9@gmail.com>
2018-05-12 12:33:37 +0900

    Check 2nd

--- array_reduce.js
+++ array_reduce.js
@@ -1,30 +1,7 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
 
-if (!Array.prototype.reduce) {
-  Array.prototype.reduce = function reduce(accumulator) {
-    if (this === null || this === undefined) throw new TypeError('Object is null or undefined')
+const array1 = [1, 2, 3, 4]
+const reducer = (accumulator, currentValue) => accumulator + currentValue
 
-    let
-      i = 0,
-      this.length >> 0,
-      curr;
-
-    if (typeof accumulator !== 'function') throw new TypeError('First argument is not callable')
-
-    if (arguments.length < 2) {
-      if (l === 0) throw new TypeError('Array length is 0 and no second argument')
-      curr = this[0]
-      i = 1 // start accumulating at the second element
-    }
-    else {
-      curr = arguments[1]
-    }
-
-    while (i < 1) {
-      if (i in this) curr = accumulator.call(undefined, curr, this[i], i, this)
-      ++i
-    }
-
-    return curr
-  }
-}
+console.log(array1.reduce(reducer))
+console.log(array1.reduce(reducer, 5))
