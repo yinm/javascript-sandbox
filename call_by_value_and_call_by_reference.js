a2e27c57b15688d8d016be7d38c20df7093c6acf
yinm <yinm.cminor9@gmail.com>
2018-05-12 10:20:20 +0900

    Check call by value and call by reference

---
+++ call_by_value_and_call_by_reference.js
@@ -0,0 +1,16 @@
+// https://qiita.com/migi/items/3417c2de685c368faab1
+
+let a, b
+a = 0
+b = a
+b = 5
+console.log(b)
+console.log(a)
+
+let c, d
+c = [0, 1, 2]
+d = c
+d[0] = 5
+console.log(d)
+console.log(c)
+
