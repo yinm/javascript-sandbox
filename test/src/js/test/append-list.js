c6961f1d92f6502b955f9c4db622799e572f745f
yinm <yinm.cminor9@gmail.com>
2018-05-12 16:42:28 +0900

    Check html2js preprocessor

--- test/src/js/test/append-list.js
+++ test/src/js/test/append-list.js
@@ -1,17 +1,21 @@
 import assert from 'assert'
 import { appendList } from "../append-list"
 
-document.body.innerHTML = '<ul class="list"></ul>'
+describe('appendList', () => {
+  it('コンテナにリストを追加できる', () => {
+    document.body.innerHTML = __html__['src/js/test/append-list.html']
 
-let ul = document.querySelector('.list')
-assert.equal(ul.children.length, 0, '最初は0件')
+    let ul = document.querySelector('.list')
+    assert.equal(ul.children.length, 0, '最初は0件')
 
-appendList(ul, '要素1')
-assert.equal(ul.children.length, 1, '件数が1件に!')
-assert.equal(ul.children[0].textContent, '要素1')
-assert.equal(ul.children[0].outerHTML, '<li>要素1</li>')
+    appendList(ul, '要素1')
+    assert.equal(ul.children.length, 1, '件数が1件に!')
+    assert.equal(ul.children[0].textContent, '要素1')
+    assert.equal(ul.children[0].outerHTML, '<li>要素1</li>')
 
-appendList(ul, 'リスト2')
-assert.equal(ul.children.length, 2, '件数が2件に!!')
-assert.equal(ul.children[0].textContent, '要素1')
-assert.equal(ul.children[1].textContent, 'リスト2')
+    appendList(ul, 'リスト2')
+    assert.equal(ul.children.length, 2, '件数が2件に!!')
+    assert.equal(ul.children[0].textContent, '要素1')
+    assert.equal(ul.children[1].textContent, 'リスト2')
+  })
+})
