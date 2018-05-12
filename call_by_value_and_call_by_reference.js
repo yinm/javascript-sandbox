3fefc214ca61c8e4da1c54173f5e254b842f4237
yinm <yinm.cminor9@gmail.com>
2018-05-12 10:38:29 +0900

    Check call by value with string

--- call_by_value_and_call_by_reference.js
+++ call_by_value_and_call_by_reference.js
@@ -1,15 +1,12 @@
 // https://qiita.com/migi/items/3417c2de685c368faab1
 
 let a, b
-a = {x: 0, y: 0}
-console.log(a)
-
-b = JSON.stringify(a)
-console.log(b)
-
-b = JSON.parse(b)
-console.log(b)
-
-b.x = 5
+a = 'hoge'
+b = a
+b = 'fuga'
 console.log(b)
 console.log(a)
+
+a = 'hogehoge'
+console.log(a)
+console.log(b)
