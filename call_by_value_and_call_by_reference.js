9cd42c13d14b215b87380dd7384d84f151f36dfb
yinm <yinm.cminor9@gmail.com>
2018-05-12 10:30:35 +0900

    Check value assigned to primitive will be lost

--- call_by_value_and_call_by_reference.js
+++ call_by_value_and_call_by_reference.js
@@ -1,9 +1,14 @@
 // https://qiita.com/migi/items/3417c2de685c368faab1
 
 let a, b
-a = {hoge: 'hogehoge', fuga: 'fugafuga'}
+a = 0
 b = a
-// b = {}
-b = new Object()
-console.log(b)
-console.log(a)
+b.hoge = 'hoge'
+
+console.log(a.hoge)
+console.log(b.hoge)
+
+let c
+c = 0
+c.hoge = 'hoge'
+console.log(c.hoge)
