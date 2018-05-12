0edd604a92f3385d11eb47ed9e8928f317536f55
yinm <yinm.cminor9@gmail.com>
2018-05-12 11:11:01 +0900

    Check spread syntax with array.unshift()

--- spread_syntax.js
+++ spread_syntax.js
@@ -1,11 +1,7 @@
 // https://qiita.com/sanonosa/items/e6f503cbb95c8e6967f8
 
 let data1 = [1, 2, 3]
-let data1a = [1, 2, 3]
 let data2 = ['d', 'e', 'f']
 
-Array.prototype.push.apply(data1, data2)
+data1.unshift(...data2)
 console.log(data1)
-
-console.log([].push.apply(data1a, data2))
-console.log(data1a)
