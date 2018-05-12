ae640cca18706b1f3be86bd313ce96c892b6fee2
yinm <yinm.cminor9@gmail.com>
2018-05-12 11:09:56 +0900

    Check old pattern

--- spread_syntax.js
+++ spread_syntax.js
@@ -1,7 +1,11 @@
 // https://qiita.com/sanonosa/items/e6f503cbb95c8e6967f8
 
 let data1 = [1, 2, 3]
+let data1a = [1, 2, 3]
 let data2 = ['d', 'e', 'f']
 
-data1.push(...data2)
+Array.prototype.push.apply(data1, data2)
 console.log(data1)
+
+console.log([].push.apply(data1a, data2))
+console.log(data1a)
