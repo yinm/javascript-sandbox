a815549f3f6974441294ec5d5921faf71dd16ab4
yinm <yinm.cminor9@gmail.com>
2018-05-12 11:05:11 +0900

    Check shallow copy

--- spread_syntax.js
+++ spread_syntax.js
@@ -1,11 +1,12 @@
 // https://qiita.com/sanonosa/items/e6f503cbb95c8e6967f8
 
-const array = ['Pen', 'Pineapple', 'apple']
-const myArray = [...array]
+let fruits = [
+  {'banana': 100},
+  {'cherry': 200},
+]
+let myFruits = [...fruits]
+console.log(myFruits)
 
-console.log(array === myArray)
-console.log(myArray)
-
-myArray[1] = 'ぱいなっぷる'
-console.log(myArray)
-console.log(array)
+fruits[0].banana = 300
+console.log(fruits)
+console.log(myFruits)
