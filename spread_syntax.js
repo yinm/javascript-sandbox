49be4e24e610fee33ab5cb912c422ba0e451cdc0
yinm <yinm.cminor9@gmail.com>
2018-05-12 11:23:29 +0900

    Check removing duplication

--- spread_syntax.js
+++ spread_syntax.js
@@ -1,11 +1,6 @@
 // https://qiita.com/sanonosa/items/e6f503cbb95c8e6967f8
 
-const word = 'プログラミングJavaScript';
+const data = ['a', 'b', 'c', 'a', 'b', 'd']
+const dist = [...(new Set(data))]
 
-[...word].forEach(c => console.log(c))
-
-const reversed = [...word].reduceRight((p, c) => p + c)
-console.log(reversed)
-
-const reversed2 = [...word].reverse().join('')
-console.log(reversed2)
+console.log(dist)
