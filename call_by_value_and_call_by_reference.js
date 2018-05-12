486ec9e8853a1d161ef84c1e240a3a9131fda5fe
yinm <yinm.cminor9@gmail.com>
2018-05-12 10:22:22 +0900

    Check call by reference

--- call_by_value_and_call_by_reference.js
+++ call_by_value_and_call_by_reference.js
@@ -1,16 +1,8 @@
 // https://qiita.com/migi/items/3417c2de685c368faab1
 
 let a, b
-a = 0
+a = {x: 0, y: 0}
 b = a
-b = 5
+b.x = 5
 console.log(b)
 console.log(a)
-
-let c, d
-c = [0, 1, 2]
-d = c
-d[0] = 5
-console.log(d)
-console.log(c)
-
