2ec65400c6480704a76ddcefcecf3738b7681dc7
yinm <yinm.cminor9@gmail.com>
2018-05-12 12:30:25 +0900

    Check new operator with spread syntax

--- spread_syntax2.js
+++ spread_syntax2.js
@@ -1,10 +1,5 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Spread_syntax
 
-function sum(x, y, z) {
-  return x + y + z
-}
-
-const numbers = [1, 2, 3]
-
-console.log(sum(...numbers))
-console.log(sum.apply(null, numbers))
+const dateFields = [1970, 0, 1]
+const d = new Date(...dateFields)
+console.log(d)
