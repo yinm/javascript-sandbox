02ba1ec4baa37a3f5b9bb6ec58a0cb605aefcadc
yinm <yinm.cminor9@gmail.com>
2018-05-12 11:21:21 +0900

    Check converting string to array

--- spread_syntax.js
+++ spread_syntax.js
@@ -1,6 +1,11 @@
 // https://qiita.com/sanonosa/items/e6f503cbb95c8e6967f8
 
-const word = 'JavaScriptプログラミング'
-const converted = [...word]
+const word = 'プログラミングJavaScript';
 
-console.log(converted)
+[...word].forEach(c => console.log(c))
+
+const reversed = [...word].reduceRight((p, c) => p + c)
+console.log(reversed)
+
+const reversed2 = [...word].reverse().join('')
+console.log(reversed2)
