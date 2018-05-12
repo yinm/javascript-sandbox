0277009fbafe66555328e182c7783862ff7f0907
yinm <yinm.cminor9@gmail.com>
2018-05-12 10:03:30 +0900

    Check copying accessors

--- object_assign.js
+++ object_assign.js
@@ -1,17 +1,35 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
 
-let target = Object.defineProperty({}, 'foo', {
-  value: 1,
-  writable: false
-})
-
-try {
-  Object.assign(target, {bar: 2}, {foo2: 3, foo: 3, foo3: 3}, {baz: 4})
-} catch(e) {
-  console.log(target.bar)
-  console.log(target.foo2)
-  console.log(target.foo)
-  console.log(target.foo3)
-  console.log(target.baz)
+const obj = {
+  foo: 1,
+  get bar() {
+    return 2
+  }
 }
 
+let copy = Object.assign({}, obj)
+console.log(copy)
+
+function completeAssign(target, ...sources) {
+  sources.forEach(source => {
+    let descriptors = Object.keys(source).reduce((descriptors, key) => {
+      descriptors[key] = Object.getOwnPropertyDescriptor(source, key)
+      return descriptors
+    }, {})
+
+    // by default, Object.assign copies enumerable Symbols too
+    Object.getOwnPropertySymbols(source).forEach(sym => {
+      let descriptor = Object.getOwnPropertyDescriptor(source, sym)
+      if (descriptor.enumerable) {
+        descriptors[sym] = descriptor
+      }
+    })
+
+    Object.defineProperties(target, descriptors)
+  })
+
+  return target
+}
+
+copy = completeAssign({}, obj)
+console.log(copy)
