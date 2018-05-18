7382816043c6554d6a2699ea518ce85a55b851b1
yinm <yinm.cminor9@gmail.com>
2018-05-18 22:08:25 +0900

    Add presentational components

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -1,72 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { Provider, connect } from 'react-redux'
 
-class AppComponents extends React.Component {
+class AppComponent extends React.Component {
+  constructor() {
+    super()
+    this.send = this.send.bind(this)
+  }
+
   send(e) {
-    this.props.onClick(this.refs.inputText.value)
+    this.props.onclick(this.refs.inputText.value)
   }
 
   render() {
     return (
       <div>
         <input type="text" defaultValue="" ref="inputText" />
-        <button onClick={this.send.bind(this)}>計算</button>
+        <button onClick={this.send}>計算</button>
         <br />
         {this.props.price}
       </div>
     )
   }
 }
-
-function mapStateToProps(state) {
-  return {
-    price: state.price
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    onClick(price) {
-      dispatch(addTax(price))
-    }
-  }
-}
-
-const AppContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(AppComponents)
-
-const ADDTAX = 'ADDTAX'
-function addTax(price) {
-  return {
-    type: ADDTAX,
-    price
-  }
-}
-
-function addReducer(state, action) {
-  switch (action.type) {
-    case 'ADDTAX':
-      return (
-        Object.assign({}, state, {price: action.price * 1.08})
-      )
-    default:
-      return state
-  }
-}
-
-const initialState = {
-  price: ''
-}
-
-const store = createStore(addReducer, initialState)
-
-ReactDOM.render(
-  <Provider store={store}>
-    <AppContainer/>
-  </Provider>,
-  document.getElementById('root')
-)
