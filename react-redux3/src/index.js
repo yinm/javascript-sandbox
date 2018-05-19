3fb839e373780e4955f5d160701f2fb574487269
yinm <yinm.cminor9@gmail.com>
2018-05-19 09:28:17 +0900

    Use react-redux

--- react-redux3/src/index.js
+++ react-redux3/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './app'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-render(<App/>, document.getElementById('root'))
+import App from './container/app'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+)
