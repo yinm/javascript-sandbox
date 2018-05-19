3f252edaff13a1b756a623d9cd7a8d4afebf8262
yinm <yinm.cminor9@gmail.com>
2018-05-19 09:37:00 +0900

    Add action

--- react-redux3/src/container/app.jsx
+++ react-redux3/src/container/app.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import App from '../component/app'
+import { increment } from '../action/app'
 
 function mapStateToProps(state) {
   return state
 }
 
-export default connect(mapStateToProps)(App)
+function mapDispatchToProps(dispatch) {
+  return {
+    handleClick: () => { dispatch(increment()) }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
