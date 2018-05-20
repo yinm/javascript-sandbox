aef09de4f452224a88a4da94d0594b89dbb61404
yinm <yinm.cminor9@gmail.com>
2018-05-20 21:22:25 +0900

    Check f2c

--- string_replace2.js
+++ string_replace2.js
@@ -1,13 +1,13 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/replace
 
-function styleHyphenFormat(propertyName) {
-  function upperToHyphenLower(match, offset, string) {
-    console.log(offset)
-    return (offset > 0 ? '-' : '') + match.toLowerCase()
+function f2c(x) {
+  function convert(str, p1, offset, s) {
+    return ((p1 - 32) * 5/9) + 'C'
   }
 
-  return propertyName.replace(/[A-Z]/g, upperToHyphenLower)
+  let s = String(x)
+  const test = /(-?\d+(?:\.\d*)?)F\b/g
+  return s.replace(test, convert)
 }
 
-console.log(styleHyphenFormat('borderTop'))
-console.log(styleHyphenFormat('ToUpperCase'))
+console.log(f2c('212F'))
