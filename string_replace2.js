e7b3030c76b4703cc71d09834b9ec3268e1cadd2
yinm <yinm.cminor9@gmail.com>
2018-05-20 20:58:43 +0900

    Check regexp

--- string_replace2.js
+++ string_replace2.js
@@ -1,12 +1,5 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/replace
 
-function replacer(match, p1, p2, p3, offset, string) {
-  console.log(match)
-  console.log(offset)
-  console.log(string)
-
-  return [p1, p2, p3].join(' - ')
-}
-
-const newString = 'abc12345#$*%'.replace(/([^\d]*)(\d*)([^\w]*)/, replacer)
-console.log(newString)
+const str = 'Twas the night before Xmas...'
+const newStr = str.replace(/xmas/i, 'Christmas')
+console.log(newStr)
