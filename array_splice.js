6c744782d1115f8126787f865f3cc72b38072630
yinm <yinm.cminor9@gmail.com>
2018-05-20 20:39:50 +0900

    Check replace

--- array_splice.js
+++ array_splice.js
@@ -1,7 +1,7 @@
 //ref: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
 
-let myFish = ['1', '2', '3', '4', '5']
-const removed = myFish.splice(3, 1)
+let myFish = ['angel', 'clown', 'drum', 'sturgeon']
+const removed = myFish.splice(2, 1, 'trumpet')
 
 console.log(myFish)
 console.log(removed)
