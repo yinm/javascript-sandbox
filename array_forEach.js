4a7e895d8346bbca0560d0a44ff867583fb97d70
yinm <yinm.cminor9@gmail.com>
2018-05-20 20:31:47 +0900

    Check polyfill

--- array_forEach.js
+++ array_forEach.js
@@ -1,11 +1,36 @@
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 
-let words = ['one', 'two', 'three', 'four']
-words.forEach((word) => {
-  console.log(word)
-  if (word === 'two') {
-    words.shift()
-  }
-})
+if (!Array.prototype.forEach) {
+  Array.prototype.forEach = function(callback/*, thisArg*/) {
+    let T, k
 
-console.log(words)
+    if (this == null) {
+      throw new TypeError('this is null or not defined')
+    }
+
+    let O = Object(this)
+
+    let len = O.length >>> 0
+
+    if (typeof callback !== 'function') {
+      throw new TypeError(callback + 'is not a function')
+    }
+
+    if (arguments.length > 1) {
+      T = arguments[1]
+    }
+
+    k = 0
+
+    while (k < len) {
+      let kValue
+
+      if (k in O) {
+        kValue = O[k]
+        callback.call(T, kValue, k, O)
+      }
+
+      k++
+    }
+  }
+}
