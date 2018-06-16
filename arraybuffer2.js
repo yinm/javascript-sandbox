096ef4d579fa84cf7fc1ec3d42a3423c4c369e82
yinm <yinm.cminor9@gmail.com>
2018-06-16 21:17:59 +0900

    Check DataView

--- arraybuffer2.js
+++ arraybuffer2.js
@@ -1,18 +1,23 @@
 // https://qiita.com/edo_m18/items/612d2b31498d22d13b7b
 
 const buffer = new ArrayBuffer(4 + 2 + 4)
+const view = new DataView(buffer)
+let offset = 0
 
-let aryU16 = new Uint16Array(buffer, 0, 2)
-aryU16[0] = 0x0001
-aryU16[1] = 0x0100
+view.setUint16(offset, 0x0001, true)
+offset += 2
+view.setUint16(offset, 0x0100, true)
+offset += 2
 
-let aryU8 = new Uint8Array(buffer, 4, 2)
-aryU8[0] = 0xff
-aryU8[1] = 0x0f
+view.setUint8(offset, 0xff)
+offset += 1
+view.setUint8(offset, 0x0f)
+offset += 1
 
-let aryI16 = new Int16Array(buffer, 6, 2)
-aryI16[0] = -50
-aryI16[1] = -30
+view.setInt16(offset, -50, true)
+offset += 2
+view.setUint16(offset, -30, true)
+offset += 2
 
 const result = new Uint8Array(buffer)
 console.log(result)
