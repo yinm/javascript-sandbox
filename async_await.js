5a0ad625478430b7345fd7e4038824bfdb5a9ab3
yinm <yinm.cminor9@gmail.com>
2018-06-16 12:25:21 +0900

    Check 2nd

--- async_await.js
+++ async_await.js
@@ -12,17 +12,23 @@ function sampleResolve2(value) {
   return new Promise(resolve => {
     setTimeout(() => {
       resolve(value * 2)
-    }, 500)
+    }, 400)
   })
 }
 
 function sample() {
   const promiseA = sampleResolve(5)
   const promiseB = sampleResolve(10)
-  const promiseC = promiseB.then(value => sampleResolve2(value))
+  const promiseC = promiseB.then(value => {
+    return sampleResolve2(value)
+  })
 
   return Promise.all([promiseA, promiseB, promiseC])
-    .then(([a, b, c]) => [a, b, c])
+    .then(([a, b, c]) => {
+      return [a, b, c]
+    })
 }
 
-sample().then(([a, b, c]) => console.log(a, b, c))
+sample().then(([a, b, c]) => {
+  console.log(a, b, c)
+})
