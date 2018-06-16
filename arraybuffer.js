0b9ac30a1e69cbfe7c0989bfcf2251840087fe49
yinm <yinm.cminor9@gmail.com>
2018-06-16 21:03:35 +0900

    Check ArrayBuffer

--- arraybuffer.js
+++ arraybuffer.js
@@ -1,15 +1,22 @@
 // https://qiita.com/megadreams14/items/dded3cf770010bb8ff08
 
-const buffer = new ArrayBuffer(2)
+const buffer = new ArrayBuffer(4)
 const dv = new DataView(buffer)
 
-dv.setUint8(0, 12)
-dv.setUint8(1, 3)
+dv.setUint16(0, 2017, true)
+dv.setUint8(2, 12)
+dv.setUint8(3, 3)
 
-const month = dv.getUint8(0)
+console.log(dv.getUint8(0).toString(2))
+console.log(dv.getUint8(1).toString(2))
+console.log(dv.getUint8(2).toString(2))
+console.log(dv.getUint8(3).toString(2))
+
+const year = dv.getUint16(0, true)
+console.log(year)
+
+const month = dv.getUint8(2)
 console.log(month)
-console.log(month.toString(2))
 
-const day = dv.getUint8(1)
+const day = dv.getUint8(3)
 console.log(day)
-console.log(day.toString(2))
