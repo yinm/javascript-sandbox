c519c3c925216da21c058689eac0cbaea1a2fb23
yinm <yinm.cminor9@gmail.com>
2018-06-16 11:55:18 +0900

    Check async

--- async_await.js
+++ async_await.js
@@ -1,20 +1,17 @@
 // https://qiita.com/soarflat/items/1a9613e023200bbebcb3
 
-function throwError() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      try {
-        throw new Error('Error occurred')
-        resolve('No error')
-      } catch(err) {
-        reject(err)
-      }
-    }, 1000)
-  })
+async function resolveSample() {
+  return 'resolve!!'
 }
+resolveSample().then(value => console.log(value))
 
-async function errorHandling() {
-  return await throwError()
+async function rejectSample() {
+  throw new Error('reject!!')
 }
+rejectSample().catch(err => console.log(err))
 
-errorHandling().catch(err => console.log(err))
+// can't execute
+// function resolveError() {
+//   return `resolveError`
+// }
+// resolveError().then(value => console.log(value))
