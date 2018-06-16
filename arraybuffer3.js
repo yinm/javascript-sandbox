6fbd615ff3d16ab70336e4eceb4d1843845feefb
yinm <yinm.cminor9@gmail.com>
2018-06-16 22:18:46 +0900

    Check IEEE-11073 32-bit SFLOAT

--- arraybuffer3.js
+++ arraybuffer3.js
@@ -1,27 +1,22 @@
 // https://qiita.com/megadreams14/items/1c88e71d87970bc8ab90
 
-function readFLOAT(dv, isLittleEndian = false) {
-  const data = dv.getUint32(0, isLittleEndian)
-  let mantissa = (data & 0x00FFFFFF)
+function readSFLOAT(dv, isLitterEndian = false) {
+  const data = dv.getUint16(0, isLitterEndian)
+  let mantissa = (data & 0x0FFF)
 
-  if ((mantissa & 0x00800000) > 0) {
-    mantissa = -1 * (~(mantissa - 0x01) & 0x00FFFFFF)
+  if ((mantissa & 0x0800) > 0) {
+    mantissa = -1 * (~(mantissa - 0x01) & 0x0FFF)
   }
 
-  const exponential = data >> 24
+  const exponential = data >> 12
 
   return mantissa * Math.pow(10, exponential)
 }
 
-const bufferA = new ArrayBuffer(4)
-let dvA = new DataView(bufferA)
-dvA.setUint32(0, 0x690100FF)
-let isLittleEndian = true
-const resultA = readFLOAT(dvA, isLittleEndian)
-console.log(resultA)
+const buffer = new ArrayBuffer(2)
+let dv = new DataView(buffer)
+dv.setUint16(0, 0x7200)
+const isLitterEndian = true
 
-const bufferB = new ArrayBuffer(4)
-let dvB = new DataView(bufferB)
-dvB.setUint32(0, 0x1F0E00FE)
-const resultB = readFLOAT(dvB, isLittleEndian)
-console.log(resultB)
+const result = readSFLOAT(dv, isLitterEndian)
+console.log(result)
