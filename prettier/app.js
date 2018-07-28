40e98bdb1b39f2d1b703d7b7f3e72cacd1859216
yinm <yinm.cminor9@gmail.com>
2018-07-28 14:30:17 +0900

    npx prettier app.js --write

--- prettier/app.js
+++ prettier/app.js
@@ -1,27 +1,24 @@
-class Person{
+class Person {
   constructor(name) {
-    this.name =   name;
+    this.name = name;
   }
-
 }
-
 
 const profile = {
-  name: 'soarflat', sex: 'male', location: 'Tokyo'
-
+  name: "soarflat",
+  sex: "male",
+  location: "Tokyo"
 };
 
-const hoge = ()=>{
-  console.log('hoge');
-}
-
-const fooBar = (foo, bar) =>{
-    console.log(foo);
-  console.log(bar)
+const hoge = () => {
+  console.log("hoge");
 };
 
-fooBar(111,
-    {
-        hoge: 'hoge!'
-    }
-)
+const fooBar = (foo, bar) => {
+  console.log(foo);
+  console.log(bar);
+};
+
+fooBar(111, {
+  hoge: "hoge!"
+});
