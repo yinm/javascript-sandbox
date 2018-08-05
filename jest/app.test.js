5339f4e740500d2f4037d4e745abf5a18a0699c4
yinm <yinm.cminor9@gmail.com>
2018-08-05 12:41:09 +0900

    Check tests about number

--- jest/app.test.js
+++ jest/app.test.js
@@ -1,17 +1,10 @@
-test('null', () => {
-  const n = null
-  expect(n).toBeNull()
-  expect(n).toBeDefined()
-  expect(n).not.toBeUndefined()
-  expect(n).not.toBeTruthy()
-  expect(n).toBeFalsy()
-})
+test('two plus two', () => {
+  const value = 2 + 2
+  expect(value).toBeGreaterThan(3)
+  expect(value).toBeGreaterThanOrEqual(3.5)
+  expect(value).toBeLessThan(5)
+  expect(value).toBeLessThanOrEqual(4.5)
 
-test('zero', () => {
-  const z = 0
-  expect(z).not.toBeNull()
-  expect(z).toBeDefined()
-  expect(z).not.toBeUndefined()
-  expect(z).not.toBeTruthy()
-  expect(z).toBeFalsy()
+  expect(value).toBe(4)
+  expect(value).toEqual(4)
 })
