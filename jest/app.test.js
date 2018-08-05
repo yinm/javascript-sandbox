d6c5e6474f207c19e3fc06c4632cb57b3efcbf4c
yinm <yinm.cminor9@gmail.com>
2018-08-05 13:04:35 +0900

    Check test of callback

--- jest/app.test.js
+++ jest/app.test.js
@@ -1,22 +1,14 @@
-function ConfigError(message) {
-  this.message = message
-  const lastPart = new Error().stack.match(/[^\s]+$/)
-  this.stack = `${this.name} at ${lastPart}`
-}
-Object.setPrototypeOf(ConfigError, Error)
-ConfigError.prototype = Object.create(Error.prototype)
-ConfigError.prototype.name = 'ConfigError'
-ConfigError.prototype.message = ''
-ConfigError.prototype.constructor = ConfigError
-
-function compileAndroidCode() {
-  throw new ConfigError('you are using the wrong JDK')
+function fetchData(cb) {
+  setTimeout(() => {
+    cb('peanut butter')
+  }, 2000)
 }
 
-test('compiling android goes as expected', () => {
-  expect(compileAndroidCode).toThrow()
-  expect(compileAndroidCode).toThrow(ConfigError)
+test('the data is peanut butter', done => {
+  function callback(data) {
+    expect(data).toBe('peanut butter')
+    done()
+  }
 
-  expect(compileAndroidCode).toThrow('you are using the wrong JDK')
-  expect(compileAndroidCode).toThrow(/JDK/)
+  fetchData(callback)
 })
