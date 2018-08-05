8978209131f4a80157aac0e4fe0da13c39d13961
yinm <yinm.cminor9@gmail.com>
2018-08-05 12:38:36 +0900

    Check truthy and falsy

--- jest/app.test.js
+++ jest/app.test.js
@@ -1,7 +1,17 @@
-test('adding positive numbers is not zero', () => {
-  for(let a = 1; a < 10; a++) {
-    for (let b = 1; b < 10; b++) {
-      expect(a + b).not.toBe(0)
-    }
-  }
+test('null', () => {
+  const n = null
+  expect(n).toBeNull()
+  expect(n).toBeDefined()
+  expect(n).not.toBeUndefined()
+  expect(n).not.toBeTruthy()
+  expect(n).toBeFalsy()
+})
+
+test('zero', () => {
+  const z = 0
+  expect(z).not.toBeNull()
+  expect(z).toBeDefined()
+  expect(z).not.toBeUndefined()
+  expect(z).not.toBeTruthy()
+  expect(z).toBeFalsy()
 })
