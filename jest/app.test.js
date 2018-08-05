fd1e05b9123ff7e1c7bc25c87f50924c94e8a5a1
yinm <yinm.cminor9@gmail.com>
2018-08-05 13:43:30 +0900

    Check async/await

--- jest/app.test.js
+++ jest/app.test.js
@@ -1,14 +1,23 @@
-function fetchData(isRejected) {
-  return new Promise((resolve, reject) => {
-    if (isRejected) {
-      reject('error')
-    }
+async function fetchData(isRejected) {
+  if (isRejected) {
+    throw new Error('error')
+  }
 
-    resolve('peanut butter')
-  })
+  return 'peanut butter'
 }
 
-test('the fetch fails with an error', () => {
+test('the data is peanut butter', async () => {
   expect.assertions(1)
-  return expect(fetchData(true)).rejects.toMatch('error')
+  const data = await fetchData(false)
+  expect(data).toBe('peanut butter')
+})
+
+test('the fetch fails with an error', async () => {
+  expect.assertions(1)
+
+  try {
+    await fetchData(true)
+  } catch (e) {
+    expect(e.message).toMatch('error')
+  }
 })
