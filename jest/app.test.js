9f84916303bd96652014406773a5250c9dae0a83
yinm <yinm.cminor9@gmail.com>
2018-08-05 13:27:16 +0900

    Check resolve

--- jest/app.test.js
+++ jest/app.test.js
@@ -1,14 +1,16 @@
-function fetchData(cb) {
-  setTimeout(() => {
-    cb('peanut butter')
-  }, 2000)
+function fetchData(isRejected) {
+  return new Promise((resolve, reject) => {
+    if (isRejected) {
+      reject(new Error('error'))
+    }
+
+    resolve('peanut butter')
+  })
 }
 
-test('the data is peanut butter', done => {
-  function callback(data) {
+test('the data is peanut butter', () => {
+  expect.assertions(1)
+  return fetchData(false).then(data => {
     expect(data).toBe('peanut butter')
-    done()
-  }
-
-  fetchData(callback)
+  })
 })
