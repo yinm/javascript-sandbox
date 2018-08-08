463dc2ff82b261427924f198e51ab46b3f93ed46
yinm <yinm.cminor9@gmail.com>
2018-08-08 21:22:51 +0900

    Add rule

--- eslint3/eslint/rule/no-literal-call.js
+++ eslint3/eslint/rule/no-literal-call.js
@@ -1,9 +1,23 @@
 'use strict'
 
+const LITERAL_TYPE = /^(?:Literal|ArrayExpression|ObjectExpression|TemplateLiteral)$/
+
 module.exports = function(context) {
   return {
-    'ArrayExpression'() {
-      console.log('配列リテラルを発見')
+    'CallExpression'(node) {
+      const callee = node.callee
+      let message = null
+
+      if (LITERAL_TYPE.test(callee.type)) {
+        message = 'This is not a function.'
+      }
+      if (callee.type === 'ClassExpression') {
+        message = 'Class constructors cannot be invoked without "new".'
+      }
+
+      if (message) {
+        context.report({node: node, message: message})
+      }
     }
   }
 }
