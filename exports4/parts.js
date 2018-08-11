87e3cc0a9417bb56aac87fa6a014dd6a15f2e68c
yinm <yinm.cminor9@gmail.com>
2018-08-11 15:08:06 +0900

    Check module.exports === exports

--- exports4/parts.js
+++ exports4/parts.js
@@ -1,7 +1,11 @@
 const value = '値'
 const foo = 'foo'
+console.log(module.exports === exports)
+
 module.exports = {
-  fuga: 'fuga',
+  fuga: 'fuga'
 }
+console.log(module.exports === exports)
 
 exports.value = value
+console.log(module.exports === exports)
