eabdf1a1baa0d054bef79ddc26f0c11b6d1c275b
yinm <yinm.cminor9@gmail.com>
2018-10-08 14:42:49 +0900

    Check

--- spread_syntax.js
+++ spread_syntax.js
@@ -1,4 +1,7 @@
 // https://qiita.com/sanonosa/items/e6f503cbb95c8e6967f8
 
-const data = [15, -3, 78, 1]
-console.log(Math.max(...data))
+const ary = ['Pen', 'Pineapple', 'apple']
+const myAry = [...ary]
+
+console.log(ary === myAry)
+console.log(myAry)
