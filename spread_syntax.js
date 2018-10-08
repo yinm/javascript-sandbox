0252d90386d04e8e26e59274d466ac2c52c603dd
yinm <yinm.cminor9@gmail.com>
2018-10-08 14:50:05 +0900

    Check destructuring assignment

--- spread_syntax.js
+++ spread_syntax.js
@@ -1,7 +1,7 @@
 // https://qiita.com/sanonosa/items/e6f503cbb95c8e6967f8
 
-const data1 = [1, 2, 3]
-const data2 = [4, 5, 6]
+const [a, b, ...other] = [1, 2, 3, 4, 5]
 
-const merged = [10, ...data1, 20, ...data2]
-console.log(merged)
+console.log(a)
+console.log(b)
+console.log(other)
