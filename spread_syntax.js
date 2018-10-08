60906ca5c3f3851d1dae29f2c2e2e03b6beb23d2
yinm <yinm.cminor9@gmail.com>
2018-10-08 14:49:24 +0900

    Check

--- spread_syntax.js
+++ spread_syntax.js
@@ -1,13 +1,7 @@
 // https://qiita.com/sanonosa/items/e6f503cbb95c8e6967f8
 
 const data1 = [1, 2, 3]
-const data2 = ['d', 'e', 'f']
+const data2 = [4, 5, 6]
 
-data1.push(...data2)
-console.log(data1)
-
-const data3 = ['g']
-
-data2.push(...data3)
-console.log(data2)
-console.log(data1)
+const merged = [10, ...data1, 20, ...data2]
+console.log(merged)
