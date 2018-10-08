b97cf4b329493a270a6a43ad11687d518404c986
yinm <yinm.cminor9@gmail.com>
2018-10-08 14:45:56 +0900

    Check munipulating array

--- spread_syntax.js
+++ spread_syntax.js
@@ -1,10 +1,13 @@
 // https://qiita.com/sanonosa/items/e6f503cbb95c8e6967f8
 
-const fruits = [
-  { 'banana': 100 },
-  { 'cherry': 200 }
-]
-const myFruits = [...fruits]
+const data1 = [1, 2, 3]
+const data2 = ['d', 'e', 'f']
 
-fruits[0].banana = 300
-console.log(myFruits)
+data1.push(...data2)
+console.log(data1)
+
+const data3 = ['g']
+
+data2.push(...data3)
+console.log(data2)
+console.log(data1)
