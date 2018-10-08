d04956fb77f7e6460430dd4164df7956968b6cf2
yinm <yinm.cminor9@gmail.com>
2018-10-08 15:02:00 +0900

    Check rest parameters

--- spread_syntax.js
+++ spread_syntax.js
@@ -1,27 +1,5 @@
 // https://qiita.com/sanonosa/items/e6f503cbb95c8e6967f8
 
-const word = 'JavaScriptプログラミング';
+const sum = (...nums) => nums.reduce((accumulator, currentValue) => accumulator + currentValue)
 
-[...word].forEach(c => console.log(c))
-
-console.log('---------------------------超えられない壁')
-
-for (let c of [...word]) {
-  console.log(c)
-}
-
-console.log('---------------------------超えられない壁')
-
-for (let i in [...word]) {
-  console.log(i)
-}
-
-console.log('---------------------------超えられない壁')
-
-const reversed = [...word].reduceRight((accumulator, currentValue, index) => {
-  console.log(index, '---------------------')
-  console.log('accumulator', accumulator)
-  console.log('currentValue', currentValue)
-  return accumulator + currentValue
-})
-console.log(reversed)
+console.log(sum(1, 2, 3, 4, 5))
