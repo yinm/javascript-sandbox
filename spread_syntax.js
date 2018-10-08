8b3ba459e3748dff1d810adb76385a05671120f1
yinm <yinm.cminor9@gmail.com>
2018-10-08 14:43:52 +0900

    Check

--- spread_syntax.js
+++ spread_syntax.js
@@ -1,7 +1,10 @@
 // https://qiita.com/sanonosa/items/e6f503cbb95c8e6967f8
 
-const ary = ['Pen', 'Pineapple', 'apple']
-const myAry = [...ary]
+const fruits = [
+  { 'banana': 100 },
+  { 'cherry': 200 }
+]
+const myFruits = [...fruits]
 
-console.log(ary === myAry)
-console.log(myAry)
+fruits[0].banana = 300
+console.log(myFruits)
