abe0e7e4046a5567267e93570e07d7b23073eb7a
yinm <yinm.cminor9@gmail.com>
2018-11-11 11:45:07 +0900

    Check computed property names - { []: value }

--- object-literal.js
+++ object-literal.js
@@ -1,19 +1,12 @@
 // ref: https://qiita.com/kura07/items/356bd37733f457d3177f
 
 const obj = {
-  0: 'value',
-  1e2: 'value',
-  1e+3: 'value',
-  1e-3: 'value',
-  1.23: 'value',
-  .456: 'value',
-  789.: 'value',
-  0xff: 'value',
-  0o77: 'value',
-  0b11: 'value',
-  0123: 'value'
+  ['pr' + 'op']: 42,
+  [1 + 2]: 42,
+  [!true]: 42,
+  [{}]: 42,
+  [function(){ return 'foo' }()]: 42,
+  [Symbol()]: 42
 }
 
 console.log(obj)
-
-for (let i in obj) console.log(i, typeof i)
