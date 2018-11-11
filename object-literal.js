234ff256a99836f8107152f86b8ae945aa172ab4
yinm <yinm.cminor9@gmail.com>
2018-11-11 12:11:32 +0900

    Check { method(){} }

--- object-literal.js
+++ object-literal.js
@@ -1,12 +1,10 @@
 // ref: https://qiita.com/kura07/items/356bd37733f457d3177f
 
-// 2nd
-const prop = 0
-const obj = { prop }
-
-with({ prop: 1 }) {
-  const anotherObj = { prop }
-  console.log(anotherObj)
+const obj = {
+  method(){ alert('!') },
+  "my method"(){ alert('!') },
+  0(){ alert('!') },
+  [!true](){ alert('!') }
 }
 
 console.log(obj)
