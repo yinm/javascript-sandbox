3d0b64fe0353ca91deb431bca42185b000a848dd
yinm <yinm.cminor9@gmail.com>
2018-11-11 11:41:40 +0900

    Check { n: value }

--- object-literal.js
+++ object-literal.js
@@ -1,10 +1,19 @@
 // ref: https://qiita.com/kura07/items/356bd37733f457d3177f
 
 const obj = {
-  "prop": 42,
-  "prop2": 42,
-  "my prop": 42,
-  "": 42
+  0: 'value',
+  1e2: 'value',
+  1e+3: 'value',
+  1e-3: 'value',
+  1.23: 'value',
+  .456: 'value',
+  789.: 'value',
+  0xff: 'value',
+  0o77: 'value',
+  0b11: 'value',
+  0123: 'value'
 }
 
 console.log(obj)
+
+for (let i in obj) console.log(i, typeof i)
