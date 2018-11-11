90f237b8761edcf997dde1a57c0d775df8022084
yinm <yinm.cminor9@gmail.com>
2018-11-11 11:36:18 +0900

    Check { "prop": value }

--- object-literal.js
+++ object-literal.js
@@ -1,11 +1,10 @@
 // ref: https://qiita.com/kura07/items/356bd37733f457d3177f
 
 const obj = {
-  prop: 42,
-  prop123: 42,
-  \u0070: 42,
-  if: 42,
-  for: 42
+  "prop": 42,
+  "prop2": 42,
+  "my prop": 42,
+  "": 42
 }
 
 console.log(obj)
