1ffe636e323f9c86d9aaf853db2105eeb67f86a7
yinm <yinm.cminor9@gmail.com>
2018-11-11 12:17:32 +0900

    Check { get prop() { return value } }

--- object-literal.js
+++ object-literal.js
@@ -1,10 +1,13 @@
 // ref: https://qiita.com/kura07/items/356bd37733f457d3177f
 
 const obj = {
-  method(){ alert('!') },
-  "my method"(){ alert('!') },
-  0(){ alert('!') },
-  [!true](){ alert('!') }
+  get prop() { return Math.random() },
+  get "my prop"() { return Math.random() },
+  get 0(){ return Math.random() },
+  get [!true]() { return Math.random() }
 }
 
-console.log(obj)
+console.log(obj.prop)
+console.log(obj['my prop'])
+console.log(obj[0])
+console.log(obj.false)
