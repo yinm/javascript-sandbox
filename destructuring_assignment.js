259f51598599274b3cae53b4586fcb40a977e994
yinm <yinm.cminor9@gmail.com>
2018-11-16 20:19:04 +0900

    Check destructuring assignment with object

--- destructuring_assignment.js
+++ destructuring_assignment.js
@@ -1,13 +1,3 @@
-let a, b
-
-[a, b] = [10, 20]
-
+({a, b} = {a: 10, b: 20})
 console.log(a)
 console.log(b)
-
-let c, d, rest
-
-[c, d, ...rest] = [10, 20, 30, 40, 50]
-console.log(c)
-console.log(d)
-console.log(rest)
