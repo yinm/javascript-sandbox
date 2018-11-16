3852dd2d625d5c8aeab90d6ffb1a510854ede995
yinm <yinm.cminor9@gmail.com>
2018-11-16 20:35:47 +0900

    Check object

--- destructuring_assignment.js
+++ destructuring_assignment.js
@@ -1,12 +1,5 @@
-function parseProtocol(url) {
-  const parsedURL = /^(\w+)\:\/\/([^\/]+)\/(.*)$/.exec(url)
-  if (!parsedURL) {
-    return false
-  }
-  console.log(parsedURL)
+const o = { p: 42, q: true }
+const { p, q } = o
 
-  const [, protocol, fullhost, fullpath] = parsedURL
-  return protocol
-}
-
-console.log(parseProtocol('https://developer.mozilla.org/en-US/Web/JavaScript?q=aaa'))
+console.log(p)
+console.log(q)
