90e84c45972062a4a073d96d288f6bc95b201779
yinm <yinm.cminor9@gmail.com>
2018-11-23 08:51:47 +0900

    Check shorthand

--- object-literal.js
+++ object-literal.js
@@ -1,12 +1,11 @@
 // ref: https://qiita.com/kura07/items/356bd37733f457d3177f
 
-const obj = {
-  ['pr' + 'op']: 42,
-  [1 + 2]: 42,
-  [!true]: 42,
-  [{}]: 42,
-  [function() { return 'foo' }()]: 42,
-  [Symbol()]: 42
+const prop = 0
+const obj = { prop }
+
+with( { prop: 1 } ){
+  const anotherObj = { prop }
+  console.log(anotherObj)
 }
 
 console.log(obj)
