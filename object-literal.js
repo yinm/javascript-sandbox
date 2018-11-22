e599ebe85ebaf3a2c8d48912efe547409fe92074
yinm <yinm.cminor9@gmail.com>
2018-11-23 08:53:55 +0900

    Check method definition

--- object-literal.js
+++ object-literal.js
@@ -1,11 +1,13 @@
 // ref: https://qiita.com/kura07/items/356bd37733f457d3177f
 
-const prop = 0
-const obj = { prop }
-
-with( { prop: 1 } ){
-  const anotherObj = { prop }
-  console.log(anotherObj)
+const obj = {
+  method() { console.log('!') },
+  'my method'() { console.log('!') },
+  0() { console.log('!') },
+  [!true]() { console.log('!') }
 }
 
-console.log(obj)
+obj.method()
+obj['my method']()
+obj['0']()
+obj.false()
