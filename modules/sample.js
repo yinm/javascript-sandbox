b5e103655cf7f202b7a921cb37f701099b2c884b
yinm <yinm.cminor9@gmail.com>
2018-11-29 16:50:40 +0900

    Check anonymous closure

---
+++ modules/sample.js
@@ -0,0 +1,20 @@
+(function() {
+  const myGrades = [93, 95, 88, 0, 55, 91]
+
+  const average = () => {
+    const total = myGrades.reduce((accumulator, item) => {
+      return accumulator + item
+    }, 0)
+
+    return `Your average grade is ${total / myGrades.length}.`
+  }
+
+  const failing = () => {
+    const failingGrades = myGrades.filter(item => item < 70)
+    return `You failed ${failingGrades.length} times.`
+  }
+
+  console.log(average())
+  console.log(failing())
+})()
+
