d7fa4e4582ae36a06cbb7fd565738286d2bd418f
yinm <yinm.cminor9@gmail.com>
2018-11-29 17:21:52 +0900

    Check revealing module pattern

--- modules/sample.js
+++ modules/sample.js
@@ -1,21 +1,25 @@
 const myGradesCalculate = (function() {
   const myGrades = [93, 95, 88, 0, 55, 91]
 
+  const average = () => {
+    const total = myGrades.reduce((accumulator, item) => {
+      return accumulator + item
+    }, 0)
+
+    return `Your average grade is ${total / myGrades.length}.`
+  }
+
+  const failing = () => {
+    const failingGrades = myGrades.filter(item => item < 70)
+    return `You failed ${failingGrades.length} times.`
+  }
+
   return {
-    average() {
-      const total = myGrades.reduce((accumulator, item) => {
-        return accumulator + item
-      }, 0)
-
-      return `Your average grade is ${total / myGrades.length}.`
-    },
-
-    failing() {
-      const failingGrades = myGrades.filter(item => item < 70)
-      return `You failed ${failingGrades.length} times.`
-    }
+    average,
+    failing,
   }
 })()
 
-console.log(myGradesCalculate.failing())
 console.log(myGradesCalculate.average())
+console.log(myGradesCalculate.failing())
+
