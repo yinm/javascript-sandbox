e8375aed91444fde9d7d1512d86ecac16533d36c
yinm <yinm.cminor9@gmail.com>
2018-12-10 21:47:37 +0900

    Check

--- object_entries.js
+++ object_entries.js
@@ -1,10 +1,19 @@
 // https://qiita.com/kws9/items/6ce80ae1c0fd28a3a9d7
 
-const obj = {
-  "key-1": "value-1",
-  "key-2": "value-2",
-  "key-3": "value-3"
-}
+const list = [
+  {
+    "key": "key-1",
+    "value": "value-1"
+  },
+  {
+    "key": "key-2",
+    "value": "value-2"
+  },
+  {
+    "key": "key-3",
+    "value": "value-3"
+  }
+]
 
-console.log(Object.entries(obj))
-console.log(Object.entries(obj).map(([key, value]) => ({ [key]: value })))
+console.log(list.reduce((obj, item) => Object.assign(obj, {[item.key]: item.value}), {}))
+console.log(list.reduce((obj, item) => ({ ...obj, [item.key]: item.value }), {}))
