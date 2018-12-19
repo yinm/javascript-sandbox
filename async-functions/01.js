82dc6280d0447924b4992574a2f3d50a4138e3da
yinm <yinm.cminor9@gmail.com>
2018-12-19 20:20:07 +0900

    Check setTimeout()

---
+++ async-functions/01.js
@@ -0,0 +1,9 @@
+'use strict'
+
+console.log('フラ')
+setTimeout(() => {
+  console.log('イド')
+  setTimeout(() => {
+    console.log('チキーン')
+  }, 2000)
+} , 1000)
---
+++ async-functions/README.md
@@ -0,0 +1,1 @@
+ref: [JavaScript(Node.js) で sleep() アラカルト - Qiita](https://qiita.com/albno273/items/c2d48fdcbf3a9a3434db)
