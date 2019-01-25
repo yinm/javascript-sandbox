3b2a9ee4aba747c5d9bc246794d5a006a2b9dc3a
yinm <yinm.cminor9@gmail.com>
2019-01-25 22:02:34 +0900

    Allow CORS

--- cors4/other.js
+++ cors4/other.js
@@ -2,6 +2,11 @@ const express = require('express')
 
 const app = express()
 app.use(express.static('public'))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 app.listen(3001)
 
 console.log('listening port 3001')
