cffa63f967f36c081e30e7135ba5a8a029291db1
yinm <yinm.cminor9@gmail.com>
2019-01-26 14:41:56 +0900

    Check cookie

--- cookie_session/routes/form.js
+++ cookie_session/routes/form.js
@@ -2,7 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/', (req, res, next) => {
-  res.redirect(`http://localhost:3000/?my_id=${req.body.my_id}`)
+  if (req.body.save === 'on') {
+    res.cookie('my_id', req.body.my_id)
+  } else {
+    res.cookie('my_id', '')
+  }
+
+  res.redirect('http://localhost:3000/')
 })
 
 module.exports = router
--- cookie_session/routes/index.js
+++ cookie_session/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const myId = req.query.my_id || 'test'
+  let myId = req.cookies.my_id || 'test'
   res.render('index', { title: myId });
 });
 
